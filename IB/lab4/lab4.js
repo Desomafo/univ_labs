7b1432225873533b230e4dd95449ced01e7623a1
Камиль Камалов <kamalov-kamil@outlook.com>
2019-05-27 10:44:11 +0500

    lab4 finished

--- IB/lab4/lab4.js
+++ IB/lab4/lab4.js
@@ -4,39 +4,56 @@ function randomIntFromInterval(min,max) {
 
 function init() {
 
-    // Table initialization
-    for (i in Users) {
+    // Tables initialization
+    for (var i in Users) {
         var row = table_users_access.insertRow(i);
         row.insertCell(0).innerHTML = Users[i].name;
-        for (var j = 1; j <= ObjectsProps.length; j++) {
+        Users[i].allowed_access = randomIntFromInterval(1, 3);
+        for (var j in ObjectsProps) {
             var cell = row.insertCell(j);
-            if ()
+            if (Users[i].allowed_access == j) {
+                cell.innerHTML = "+";
+            }
         }
     }
 
-    log_in();
+    for (var i in Objects) {
+        var row = table_file_access.insertRow(i);
+        row.insertCell(0).innerHTML = Objects[i].name;
+        Objects[i].allowed_access = randomIntFromInterval(1, 3);
+        for (var j in ObjectsProps) {
+            var cell = row.insertCell(j);
+            if (Objects[i].allowed_access == j) {
+                cell.innerHTML = "+";
+            }
+        }
+    }
 
+    setTimeout(function () {
+        log_in();
+    }, 100);
+}
+
+function log_in () {
+    while (active_user == -1) {
+        var identificator = prompt("Введите идентификатор");
+        active_user = Users.findUserByName(identificator);
+        if (active_user == -1) {
+            alert("Введен неверный идентификатор\nПопробуйте еще раз");
+        }
+    }
+
+    alert("Вход совершен успешно");
+    span_user_name.innerHTML = "Пользователь: " + Users[active_user].name;
+    
     // Show user elements after success
     btn_action.style.display = "block";
     div_actions.style.display = "block";
     span_user_name.style.display = "block";
 }
 
-function log_in () {
-    while (typeof active_user === "undefined") {
-        var identificator = prompt("Введите идентификатор");
-        active_user = Users.findUserByName(identificator);
-        if (typeof active_user === "undefined") {
-            alert("Введен неверный идентификатор\nПопробуйте еще раз");
-        }
-    }
-
-    alert("Вход совершен успешно");
-    span_user_name.innerHTML = "Пользователь: " + Users[active_user];
-}
-
 function log_out () {
-    delete active_user;
+    active_user = -1;
     
     btn_action.style.display = "none";
     div_actions.style.display = "none";
@@ -53,14 +70,14 @@ var btn_action = document.getElementById("btn_action");
 var div_actions = document.getElementById("actions");
 var span_user_name = document.getElementById("user_name");
 var table_users_access = document.getElementById("users_access");
-var table_file_access = document.getElementById("file_access");
+var table_file_access = document.getElementById("files_access");
 
 // Security attrs
-var ObjectsProps = [
-    "Совершенно секретно",
-    "Секретно",
-    "Открытые данные",
-];
+var ObjectsProps = {
+    1: "Совершенно секретно",
+    2: "Секретно",
+    3: "Открытые данные",
+};
 
 // 10 Users list
 var Users = {
@@ -85,19 +102,25 @@ Users.findUserByName = function (name) {
             }
         }
     }
+    return -1;
 };
 
-var Objects = [];
-Objects.length = 5;
+var Objects = {
+    1: { name: "Файл 1" },
+    2: { name: "Файл 2" },
+    3: { name: "Файл 3" },
+    4: { name: "Файл 4" },
+    5: { name: "Файл 5" },
+};
 
-var active_user;
+var active_user = -1;
 
 
 ///////////////////
 // Event listeners
 ///////////////////
 
-btn_action.addEventListener("click", setTimeout(function () {
+btn_action.addEventListener("click", function () {
     var action = +prompt("Введите номер действия");
     if (action < 1 || action > 2) {
         alert("Неверный ввод");
@@ -105,13 +128,23 @@ btn_action.addEventListener("click", setTimeout(function () {
     }
 
     if (action == 1) {
-        var file = +prompt("К какому файлу получить доступ?"):
+        var file = +prompt("К какому файлу получить доступ?");
         if (file < 1 || file > 6) {
             alert("Выбран неверный файл");
             return;
         }
-        if (User[active_user][file])
+        if (Users[active_user].allowed_access > Objects[file].allowed_access) {
+            alert("У вас нет досупа к данному файлу");
+            return;
+        }
     }
 
+    if (action == 2) {
+        log_out();
+        setTimeout(log_in, 100);
+    }
 
-}, 50));
+    alert("Успешно");
+});
+
+document.addEventListener("load", init);
